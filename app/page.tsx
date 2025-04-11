6a1766f216aea46b5cb3374264b056ee04de23e8
wyy-07-git <wangyouyang12345@gmail.com>
2025-04-11 17:51:03 +1000

    add image

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,16 @@
 import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="bg-[url('/images/apartment-image.jpg')] bg-cover bg-center bg-no-repeat grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h1 style={{ fontFamily: "Winky Rough Italic", fontSize: "200px" }}>
+        <h1 style={{ fontFamily: "Winky Rough Italic", fontSize: "80px" }}>
           Welcome to our homepage
         </h1>
 
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
+          <li className="lists">Get started by editing .</li>
           <li className="tracking-[-.01em]">
             Save and see your changes instantly.
           </li>
