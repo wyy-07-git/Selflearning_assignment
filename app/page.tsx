3f8db69328af8deb0bd18bfc6e3fda88502bb735
wyy-07-git <wangyouyang12345@gmail.com>
2025-04-11 06:46:34 +0000

    11

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,8 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-      export default function Home() {
-        return <p className="text-blue-600 text-xl font-bold">Styled with Tailwind!</p>
-        }
+      <p className="text-blue-600 text-xl font-bold">Styled with Tailwind!</p>
+      
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
