45fc623a9f2f273d0620863bb773bb8e40c26130
wyy-07-git <wangyouyang12345@gmail.com>
2025-04-11 07:02:22 +0000

    try bont type

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 
+
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-      <p className="text-blue-600 text-xl font-bold">Styled with Tailwind!</p>
+      <h1 style={{fontFamily = 'WinkyRough-Italic-VariableFont' fontsize = 36px}}>
+        Welcome to our homepage
+      </h1>
       
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
